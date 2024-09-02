a1df348254645f05ce662e04d823aaa1afdcd66a
Matheus Valiente Souza <mvs@mvsouza.com>
2024-09-02 14:29:01 -0300

    feat: refator Dockerfile for readbility

--- docker/build.sh
+++ docker/build.sh
@@ -5,8 +5,8 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$script_dir"
 
-docker build -f Dockerfile-base -t adjoe-test/golang-base .
-docker build -f Dockerfile-dev -t adjoe-test/golang-dev .
+docker build -f Dockerfile --target base -t adjoe-test/golang-base .
+docker build -f Dockerfile --target dev -t adjoe-test/golang-dev .
 docker build -f Dockerfile-awscli -t adjoe-test/awscli .
 
 cd -
